f3582c58ee018a7d545bad1ef8e220b7b9f13d3f
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-11-05 01:38:53 +0600

    get the jobs

--- index.js
+++ index.js
@@ -2,15 +2,49 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors())
 app.use(express.json())
 
-app.get('/' , (req , res)=>{
+app.get('/', (req, res) => {
     res.send('i got the job is running')
 })
 
-app.listen(port , () =>{
-    console.log('i got the job server is running on port' , port)
+
+
+
+const uri = `mongodb+srv://igotthejob:ATzHV2DhwY34bKc8@cluster0.04lxrta.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    const jobsCollection = client.db("jobsDB").collection("jobs")
+    try {
+        app.get('/jobs', async (req, res) => {
+            const result = await jobsCollection.find().toArray();
+            res.send(result)
+          })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log('i got the job server is running on port', port)
 })
+
+
+
